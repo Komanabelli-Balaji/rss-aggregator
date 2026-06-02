1bece4209373c29e93fad97b8fd902cb28899728
Komanabelli Balaji <balajikomanabelli67@gmail.com>
2026-06-02 17:28:55 +0000

    installed go-chi router dependencies

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/go-chi/chi"
+	_ "github.com/go-chi/cors"
 )
 
 func main() {
-	fmt.Println("Hello World")
-
-	portString := os.Getenv("PORT")
-	if portString == "" {
+	err := godotenv.Load()
+	if err != nil {
 		log.Fatal("PORT is not found in the environment")
 	}
 
-	fmt.Println("POST:", portString)
+	portString := os.Getenv("PORT")
+	fmt.Println("PORT:", portString)
 }
