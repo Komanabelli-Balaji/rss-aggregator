fe0e88e22a5f8b083931ecca852d171ae9823fb9
Komanabelli Balaji <balajikomanabelli67@gmail.com>
2026-06-03 07:17:44 +0000

    boilerplate for http server

--- main.go
+++ main.go
@@ -3,19 +3,45 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
-	_ "github.com/go-chi/chi"
-	_ "github.com/go-chi/cors"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("PORT is not found in the environment")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Cannot load environmental variables")
 	}
 
 	portString := os.Getenv("PORT")
-	fmt.Println("PORT:", portString)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 		  300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	fmt.Println("Server starting on PORT:", portString)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
